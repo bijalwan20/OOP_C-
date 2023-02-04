8a855a8cdfefaf5ce18818e03076d9fc64c21fbe
bijalwan20 <102857723+bijalwan20@users.noreply.github.com>
2023-02-04 19:57:17 +0530

    Add files via upload

---
+++ Practical 4/Task3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+namespace first
+{
+int add(int a , int b)
+{
+  return (a+b);
+}
+}
+namespace second
+{
+float add(float a , float b)
+{
+  return (a+b);
+}
+}
+int main()
+{
+    cout <<"Sum is : " <<add(1,2)<<endl;
+    cout <<"Sum is : " <<add(1.2,2)<<endl;
+    cout <<"Sum is : " <<add(2,2.1)<<endl;
+    cout <<"Sum is : " <<add(1.1,2.3)<<endl;
+    cout <<"Sum is : " <<add(0,0)<<endl;
+  return 0;
+}
+
+
